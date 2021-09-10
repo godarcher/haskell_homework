df6700823bb612b249c9d84d106876a99296a53c
godarcher <joostgrunwald2001@gmail.com>
2021-09-10 16:09:59 +0200

    fixed fatal bug in number conversion

--- Say.hs
+++ Say.hs
@@ -10,6 +10,7 @@ say  5 = "five"
 say  6 = "six"
 say  7 = "seven"
 say  8 = "eight"
+say  9 = "nine"
 say 11 = "eleven"
 say 12 = "twelve"
 say 13 = "thirteen"
@@ -27,19 +28,23 @@ say 60 = "sixty"
 say 70 = "seventy"
 say 80 = "eighty"
 say 90 = "ninety"
--- say x 
---     | x < 100 = say(x - (10 * (div x 10)))
---     | x < 1000 = say(x - (100 * (div x 100)))
---     | x < 10000 = say(x - (1000 * (div x 1000)))
---     | x < 100000 = say(x - (1000 *  (div x 1000))) 
---     | x < 1000000 = say(x - (100000 * (div x 100000)))
 say x 
-    | x < 100 = say(x - mod x 10) ++ " " ++ say(x - (10 * (div x 10)))
-    | x < 1000 = say((div x 100)) ++ " hundred " ++ say(x - (100 * (div x 100)))
-    | x < 10000 = say ((div x 1000)) ++ " thousand " ++ say(x - (1000 * (div x 1000)))
-    | x < 100000 = say((div x 1000)) ++ " thousand " ++ say(x - (1000 *  (div x 1000))) 
-    | x < 1000000 = say((div x 100000)) ++ " hundred " ++ say(x - (100000 * (div x 100000)))
+     | x < 100 = say(x - mod x 10) ++" " ++ say(x - (10 * (div x 10))) 
+     | x < 1000 = say((div x 100)) ++ " hundred " ++ " REST2 " ++ show(x - (100 * (div x 100))) ++ say(x - (100 * (div x 100)))
+     | x < 10000 = say ((div x 1000)) ++ " thousand "   ++ " REST3 " ++ show(x - (1000 * (div x 1000))) ++ say(x - (1000 * (div x 1000)))
+     | x < 100000 = say((div x 1000))++ " thousand " ++ " REST4 " ++ show(x - (1000 * (div x 1000)))  ++ say(x - (1000 *  (div x 1000)))
+     | x < 1000000 = say((div x 100000)) ++ " hundred "  ++ " REST5 " ++ show(x - (100000 * (div x 100000))) ++ say(x - (100000 * (div x 100000)))
+
+
+-- DEBUG MODE
+-- say x 
+--     | x < 100 = say(x - mod x 10) ++" " ++ say(x - (10 * (div x 10))) 
+--     | x < 1000 = say((div x 100)) ++ " hundred " ++ " REST2 " ++ show(x - (100 * (div x 100))) ++ say(x - (100 * (div x 100)))
+--     | x < 10000 = say ((div x 1000)) ++ " thousand "   ++ " REST3 " ++ show(x - (1000 * (div x 1000))) ++ say(x - (1000 * (div x 1000)))
+--     | x < 100000 = say((div x 1000))++ " thousand " ++ " REST4 " ++ show(x - (1000 * (div x 1000)))  ++ say(x - (1000 *  (div x 1000)))
+--     | x < 1000000 = say((div x 100000)) ++ " hundred "  ++ " REST5 " ++ show(x - (100000 * (div x 100000))) ++ say(x - (100000 * (div x 100000)))
 
 -- 434953
 -- four hundred rest 34953
--- thirtyfour thousand 34953 - 1000 * 34 = 953
+-- thirtyfour thousand rest 953
+-- zero rest 9
