ba562a8e9479e54f52ce1d378f31e05453224cb2
godarcher <joostgrunwald2001@gmail.com>
2021-09-15 13:55:28 +0200

    added attempt at 2.3.3

--- assignment_2/Pow2.hs
+++ assignment_2/Pow2.hs
@@ -1,9 +1,15 @@
 module Pow2 where
 
-pow2 :: Integer -> Integer
-pow2 0 = 1
--- pow2 n = ... recursive case
+-- !exercise 2.3.1 and 2.3.2
 
+-- THINKING STAGE
+-- 2^0 = 1, 2^1 = 2, 2^2 = 4, 2^3 = 8, 2^4 - 16
+-- PATTERN n = 2(n-1)
 
---after finishing the function, change the type to: 
---pow2 :: (Ord n, Num n, Num a) => n -> a
+pow2 :: (Ord n, Num n, Num a) => n -> a
+pow2 0 = 1 --base case
+pow2 n = 2 * pow2(n-1) --recursive case
+
+-- !exercise 2.3.3
+-- 23
+-- TODO ADD EXPLANATION
