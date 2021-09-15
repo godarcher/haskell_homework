cbc7413db37ac6b1940c6ef1423305de711e3877
godarcher <joostgrunwald2001@gmail.com>
2021-09-15 15:30:19 +0200

    added 2.4.2

--- assignment_2/PolyType.hs
+++ assignment_2/PolyType.hs
@@ -17,3 +17,8 @@ f11 x y = get 0
 -- f10 can not be used on arguments of type string, it expects an integer input
 -- f11 can be used on arguments of type string, however it will always output the first string given to it
 -- so f8 and f11 can be used on arguments of type string
+
+-- !Exercise 2.4.2
+-- f9 and f10 are non polymorphic because they only work with one specific input type
+-- ad hoc/overloading means different behaviour for different input types, so different instances of the code
+-- because all polymorphic pieces do not display different behaviour per instance, f8 and f11 are parametric polymorphic
