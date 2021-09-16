9eeb47fce23e582f69118c65c2ed40f14473d2f7
rubenhorstman1 <ruben.horstman@student.hu.nl>
2021-09-16 14:25:06 +0200

    reverseCase done

--- assignment_2/Char.hs
+++ assignment_2/Char.hs
@@ -10,5 +10,14 @@ import Data.Char
 
 --caesar :: Int -> String -> String
 
+input :: String -> String -> Bool
+input x y  
+    | x == y = True
+    -- | otherwise = False if (x ~~ y) then True else False
+
+reverseCase :: String -> String
+reverseCase = map (\c -> if c >= 'a' && c <= 'z' then toUpper c else toLower c)
+
+
 msg :: String
 msg = "ADMNO D HPNO NKMDIFGZ TJP RDOC AVDMT YPNO"
