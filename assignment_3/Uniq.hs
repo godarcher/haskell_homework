0bc079db16f1afee1583ca5c2647ce11d6b681e5
godarcher <joostgrunwald2001@gmail.com>
2021-09-23 12:29:23 +0200

    started looking at first exercise

--- assignment_3/Uniq.hs
+++ assignment_3/Uniq.hs
@@ -1,3 +1,6 @@
 module Uniq where
 
 --uniq :: (Eq a) => [a] -> [a]
+uniq [] = [] --Base Case
+uniq [a] = [a] --Singleton case
+uniq (x:xs) = [10] --TODO: use group here for more then 1 element case???
