8f15f155df0d59b08ff7f4551004977f48bfcfc0
rubenhorstman1 <31844797+rubenhorstman1@users.noreply.github.com>
2021-09-23 15:11:47 +0200

    3.7.1 done

--- assignment_3/Lego.hs
+++ assignment_3/Lego.hs
@@ -3,7 +3,10 @@ module Lego where
 import Data.List
 import Data.Tuple
 
---removeAt :: Int -> [a] -> [a]
+removeAt :: Int -> [a] -> [a]
+removeAt y x 
+    | length x > 0 = ((take (y-1) x) ++ (take y (drop y x)))
+
 
 --sortWithPos :: (Ord a) => [a] -> [(a,Int)]
 
