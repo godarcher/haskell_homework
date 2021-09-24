4f094dd797aeaf7d8058cabfd662cef4dc5eff78
rubenhorstman1 <31844797+rubenhorstman1@users.noreply.github.com>
2021-09-24 14:15:14 +0200

    1.6.1

--- assignment_3/ListComprehensions.hs
+++ assignment_3/ListComprehensions.hs
@@ -2,14 +2,15 @@ module ListComprehensions where
 
 import Data.List
 
-g0 as bs = [ (a,b) | a <- as, b <- bs ]
+makeEveryCombination as bs = [ (a,b) | a <- as, b <- bs ] -- make a combination of every possible letter that's possible
 
-g1 n y   = [ y | i <- [1..n] ]
+makeCopy n y   = [ y | i <- [1..n] ] -- makes a copy of a number the amount of times you ask for it.
 
-g2 n xs  = [ x | (i,x) <- zip [0..] xs, i < n ]
+getLettersOfString n xs  = [ x | (i,x) <- zip [0..] xs, i < n ] --gives back the number of first letters that you request back
 
-g3 a xs  = [ i | (i,x) <- zip [0..] xs, x == a]
+givePositionBackOffChar a xs  = [ i | (i,x) <- zip [0..] xs, x == a] --gives index position of every char in string
 
-g4 xs ys = [ e | (x,y) <- zip xs ys, e <- [x,y] ]
+combineStringsOnOldPosition xs ys = [ e | (x,y) <- zip xs ys, e <- [x,y] ] --combines the letters of b oth strings together, every time the first position gets added then the second position etc.
+-- If one string is longer then a other string the string with the least characters will be the max on where it will go.
 
-g5 xss   = [ x | xs <- xss, x <- xs ]
+listToString xss   = [ x | xs <- xss, x <- xs ] --asks for a list with a string and returns a string
