03824a7dc3130cf174bd28f79546d290c2d614f8
godarcher <joostgrunwald2001@gmail.com>
2021-09-24 11:32:20 +0200

    simplified and documented 3.7.1

--- assignment_3/Lego.hs
+++ assignment_3/Lego.hs
@@ -3,9 +3,12 @@ module Lego where
 import Data.List
 import Data.Tuple
 
+
+-- ! Exercise 3.7.1
+-- * If x is not null, so the length is bigger then drop the y'th element of x
 removeAt :: Int -> [a] -> [a]
-removeAt y x 
-    | length x > 0 = ((take (y-1) x) ++ (take y (drop y x)))
+removeAt y x
+    | not(null x) = take (y-1) x ++ take y (drop y x)
 
 
 --sortWithPos :: (Ord a) => [a] -> [(a,Int)]
