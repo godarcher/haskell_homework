2888161f64e5091af7297cbd0f2164e1b5510c45
godarcher <joostgrunwald2001@gmail.com>
2021-10-03 16:05:34 +0200

    geprobeerd volgende te doen

--- assignment_4/AST.hs
+++ assignment_4/AST.hs
@@ -1,11 +1,16 @@
 module AST where
 
 --start with either:
---  data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | ...
---or:
---  data Expr = Lit Integer | Expr :+: Expr | Expr :*: Expr | ...
---  infixl 6 :+:
---  infixl 7 :*:
+data Expr = Lit Integer | Add Expr Expr | Mul Expr Expr | Sub Expr Expr | Div Expr Expr
 
 
 --eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
+
+--eval :: (Fractional a, Eq a) => Expr -> a -> Maybe a
+--eval :: Expr -> Integer
+
+eval (Lit i) = x
+eval (Add x y) = eval x + eval y
+eval (Mul x y) = eval x * eval y
+eval (Sub x y) = eval x - eval y
+eval (Div x y) = eval x / eval y
