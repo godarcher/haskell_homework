ed154b5134a9ff19bd9bf16c827eea9b5f2c47e3
godarcher <joostgrunwald2001@gmail.com>
2021-10-08 12:10:40 +0200

    finished both versions of compose

--- assignment_5/FunList.hs
+++ assignment_5/FunList.hs
@@ -1,10 +1,11 @@
 module FunList where
 
 compose :: [a -> a] -> (a -> a)
---compose [] =  
-compose (x:xs) y = x(compose xs y)
+compose [] y = y
+compose (x:xs) y = x (compose xs y)
 
---compose' :: [a -> a] -> (a -> a)
+compose' :: [a -> a] -> a -> a
+compose' = foldl (flip (.)) id
 
 --foo :: (Integral n) => n -> n
 --foo n = compose (map (*) [1..n]) 1
