14c9b2d993ef162d98049f113e9336e33986809c
godarcher <joostgrunwald2001@gmail.com>
2021-10-08 12:20:40 +0200

    finished 5.6.2

--- assignment_5/FunList.hs
+++ assignment_5/FunList.hs
@@ -9,7 +9,14 @@ compose' = foldl (flip (.)) id
 
 foo :: (Integral n) => n -> n
 foo n = compose (map (*) [1..n]) 1
--- ! explanation of foo
--- foo 1 = 1, foo 2 = 2, foo 3 = 6, foo 4 = 24, foo 5 = 120
--- foo = input number * result of previous iteration
--- so foo 5 = 5 * 24 = 120
+
+-- ! explanation of what foo does
+-- foo multiplies the set 1...n, starting with 1
+-- for n = 5, this gives 1 * 2 * 3 * 4 * 5 
+-- = 2 * 3 * 20 = 120
+-- ! explanation of how foo does this
+-- it simply takes the set (1...n) and then
+-- it uses compose with as function mapping * over 1...n
+-- and as x just the integer 1, which makes use just stack
+-- f1(f2(f3(x))) where x = 1 and function * so we basicly get:
+-- (1*(2*(3*(4*(5*(1)))))), which results in 120
