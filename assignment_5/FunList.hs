793d9839a836dd80ad0c3db0b26ea6697a70742f
godarcher <joostgrunwald2001@gmail.com>
2021-10-08 12:13:41 +0200

    finished 5.6.2a

--- assignment_5/FunList.hs
+++ assignment_5/FunList.hs
@@ -7,7 +7,9 @@ compose (x:xs) y = x (compose xs y)
 compose' :: [a -> a] -> a -> a
 compose' = foldl (flip (.)) id
 
---foo :: (Integral n) => n -> n
---foo n = compose (map (*) [1..n]) 1
-
---foldr' :: (a -> b -> b) -> b -> [a] -> b
+foo :: (Integral n) => n -> n
+foo n = compose (map (*) [1..n]) 1
+-- ! explanation of foo
+-- foo 1 = 1, foo 2 = 2, foo 3 = 6, foo 4 = 24, foo 5 = 120
+-- foo = input number * result of previous iteration
+-- so foo 5 = 5 * 24 = 120
