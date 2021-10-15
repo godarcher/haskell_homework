94edc91dd821afdca2f42e1425d719ce605e2826
godarcher <joostgrunwald2001@gmail.com>
2021-10-15 12:44:35 +0200

    got first two monoids working

--- assignment_6/BoolMonoids.hs
+++ assignment_6/BoolMonoids.hs
@@ -1,6 +1,21 @@
 module Monoids where
 import Data.Monoid
 
+-- | Boolean monoid under conjunction.
+newtype All = All { getAll :: Bool }
+        deriving (Eq, Ord, Read, Show, Bounded)
+
+instance Monoid Monoids.All where
+        mempty = Monoids.All True
+        Monoids.All x `mappend` Monoids.All y = Monoids.All (x && y)
+
+-- | Boolean monoid under disjunction.
+newtype Any = Any { getAny :: Bool }
+        deriving (Eq, Ord, Read, Show, Bounded, Generic)
+
+instance Monoid Any where
+        mempty = Monoids.Any False
+        Monoids.Any x `mappend` Monoids.Any y = Monoids.Any (x || y)
 --newtype ... = ...
 
 --instance Semigroup ... where
