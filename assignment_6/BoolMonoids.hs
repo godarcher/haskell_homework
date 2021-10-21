1a84942ccfd8faae960bdcd16e83130c7f787ce5
godarcher <joostgrunwald2001@gmail.com>
2021-10-21 20:24:28 +0200

    finished first part of third monoid

--- assignment_6/BoolMonoids.hs
+++ assignment_6/BoolMonoids.hs
@@ -8,7 +8,7 @@ newtype Conj = Conj { getConj :: Bool }
         deriving (Eq, Ord, Read, Show, Bounded)
 
 instance Semigroup Monoids.Conj where
-    Monoids.Conj f <> Monoids.Conj g = Monoids.Conj ((&&) f g)
+        Monoids.Conj f <> Monoids.Conj g = Monoids.Conj ((&&) f g)
 
 instance Monoid Monoids.Conj where
         mempty = Monoids.Conj True
@@ -19,11 +19,20 @@ newtype Disj = Disj { getDisj :: Bool }
         deriving (Eq, Ord, Read, Show, Bounded)
 
 instance Semigroup Monoids.Disj where
-    Monoids.Disj f <> Monoids.Disj g = Monoids.Disj ((||) f g)
+        Monoids.Disj f <> Monoids.Disj g = Monoids.Disj ((||) f g)
 
 instance Monoid Monoids.Disj where
         mempty = Monoids.Disj False
         Monoids.Disj x `mappend` Monoids.Disj y = Monoids.Disj (x || y) -- * Actual Disjunction
+
+-- ? EXCLUSIVE OR MONOID
+newtype ExclusiveOr = ExclusiveOr Bool
+
+instance Semigroup Monoids.ExclusiveOr where 
+        (Monoids.ExclusiveOr f) <> (Monoids.ExclusiveOr g) = Monoids.ExclusiveOr ((/=) f g)
+
+instance Monoid Monoids.ExclusiveOr where 
+        mempty = Monoids.ExclusiveOr False
 
 -- ! EXERCISE 6.5.2
 -- ? mconcat = foldr (<>) mempty
