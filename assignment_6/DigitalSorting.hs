e3a9e19bba61395889e07b954ff9e4e409383d3c
godarcher <joostgrunwald2001@gmail.com>
2021-10-21 16:14:26 +0200

    implemented 6.6.2

--- assignment_6/DigitalSorting.hs
+++ assignment_6/DigitalSorting.hs
@@ -5,21 +5,28 @@ import Data.Bool
 import Data.Maybe
 import Data.Either
 
+-- ! our extra input
+import Data.Map
 class Rankable key where
   rank :: [(key,a)] -> [[a]]
 
+-- ! Standard functions that are given
 digitalSortOn :: (Rankable key) => (v -> key) -> [v] -> [v]
-digitalSortOn f = concat . rank . map (\x->(f x, x))
+digitalSortOn f = concat . rank . Data.List.map (\x->(f x, x))
 
 digitalSort :: (Rankable key) => [key] -> [key]
 digitalSort = digitalSortOn id
 
---genericRank :: (Ord key) => [(key,a)] -> [[a]]
+-- ! Exercise 6.6.1
+genericRank :: (Ord key) => [(key,a)] -> [[a]]
+genericRank key = [b|(a, b) <- toList (fromListWith (++) [(a, [b]) | (a, b) <- key])]
 
---instance Rankable Int where ... etc.
+-- ! Exercise 6.6.2
+instance Rankable Int where
+  rank = genericRank
 
---instance Rankable Bool where ...
+instance Rankable Char where
+  rank = genericRank 
 
---instance (Rankable key1, Rankable key2) => Rankable (key1,key2) where
-
---etc.
+instance Rankable Integer where 
+  rank = genericRank
