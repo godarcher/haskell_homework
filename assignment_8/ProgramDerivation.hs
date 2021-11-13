fbc7332b95324aa5dda323c8fea22ab7afdec2c5
godarcher <joostgrunwald2001@gmail.com>
2021-11-13 14:54:00 +0100

    inorder fully done and cleaned up

--- assignment_8/ProgramDerivation.hs
+++ assignment_8/ProgramDerivation.hs
@@ -16,21 +16,11 @@ inorderCat :: Tree a -> [a] -> [a]
 inorderCat [] xs = xs
 inorderCat (Node l a r) xs = inorderCat l [a] ++ inorderCat r xs
 
--- ? The new main ordering function
-inorderFast :: Tree a -> [a]
-inorderFast inp = inorderCat inp []
 {-
-inorderCat EMPTY xs = [] ++ xs
-
-inorderCat (Node l a r) xs = inorder l ++ [a] ++ inorder r ++ xs =
-induction hypothesis tells us the equivalence holds for tree l and r
-inorderCat l [a] ++ inorderCat r xs
-
+TODO: do tests here
 -}
 
-inorderCat :: Tree a -> [a] -> [a]
-inorderCat t xs = inorder t ++ xs -- TODO: make me more efficient
-
+-- ? The new main ordering function
 inorder' :: Tree a -> [a]
 inorder' t = inorderCat t []
 
