482186411cc774dba900cf5577d60f3d82d944cb
godarcher <joostgrunwald2001@gmail.com>
2021-11-13 14:52:49 +0100

    added first implementation of inorderCat

--- assignment_8/ProgramDerivation.hs
+++ assignment_8/ProgramDerivation.hs
@@ -11,9 +11,21 @@ inorder :: Tree a -> [a]
 inorder Leaf = []
 inorder (Node x lt rt) = inorder lt ++ [x] ++ inorder rt
 
+-- ! The new upgraded helper function
+inorderCat :: Tree a -> [a] -> [a]
+inorderCat [] xs = xs
+inorderCat (Node l a r) xs = inorderCat l [a] ++ inorderCat r xs
+
+-- ? The new main ordering function
+inorderFast :: Tree a -> [a]
+inorderFast inp = inorderCat inp []
 {-
-Derive
-  inorderCat t xs = inorder t ++ xs
+inorderCat EMPTY xs = [] ++ xs
+
+inorderCat (Node l a r) xs = inorder l ++ [a] ++ inorder r ++ xs =
+induction hypothesis tells us the equivalence holds for tree l and r
+inorderCat l [a] ++ inorderCat r xs
+
 -}
 
 inorderCat :: Tree a -> [a] -> [a]
