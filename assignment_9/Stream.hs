1e70d1f8e006dd1a1ca875f87e410cb9b7ff0dab
godarcher <joostgrunwald2001@gmail.com>
2021-11-18 10:05:55 +0100

    fixed till 9.4.3

--- assignment_9/Stream.hs
+++ assignment_9/Stream.hs
@@ -15,29 +15,57 @@ instance (Show a) => Show (Stream a) where
 from :: Integer -> Stream Integer
 from n = n :> from (n + 1)
 
+-- ! Exercise 9.4.1
 -- Get only the first element
 head :: Stream a -> a
 head (h :> _) = h
 
 -- Drop only the first element
-tail :: Stream a -> Stream a  
+tail :: Stream a -> Stream a
 tail (_ :> t) = t
 
---repeat :: a -> Stream a
+-- ! Exercise 9.4.2
+-- Infinitely repeat the element
+repeat :: a -> Stream a
+repeat a = a :> repeat a
 
---map :: (a -> b) -> (Stream a -> Stream b)
+-- map function
+map :: (a -> b) -> (Stream a -> Stream b)
+map func (x :> xs) = func x :> map func xs
 
---zipWith :: (a -> b -> c) -> (Stream a -> Stream b -> Stream c)
+-- Fuse two streams, with function f
+zipWith :: (a -> b -> c) -> Stream a -> Stream b -> Stream c
+zipWith f (x :> xs) (y :> ys) = f x y :> zipWith f xs ys
 
---filter :: (a -> Bool) -> Stream a -> Stream a
+-- A pred is used to filter a stream
+filter :: (a -> Bool) -> Stream a -> Stream a
+filter a (x :> xs)
+    | a x = x :> filter a xs
+    | otherwise = filter a xs
 
+-- ! Exercise 9.4.3 
+-- The terminal gets stuck after printing an opening bracket.
+-- It most likely got stuck in an infinite loop because it 
+-- keeps on skipping element for infinity 
+
+-- ! Exercise 9.4.4
 --toList :: Stream a -> [a]
 
---cycle :: [a] -> Stream a
+cycle :: [a] -> Stream a
+cycle xs = foldr (:>) (cycle xs) xs
+--nat, fib :: Stream Integer
+--nat = 0 :> zipWith (+) nat (repeat 1)
+--fib = 0 :> 1 :> zipWith (+) fib (tail fib)
+fibS :: Stream Integer
+fibS = 0 :> (1 :> (f 0 1))
+    where f x y = (x+y) :> (f y (x+y))
 
-nat, fib :: Stream Integer
-nat = 0 :> zipWith (+) nat (repeat 1)
-fib = 0 :> 1 :> zipWith (+) fib (tail fib)
+-- | The stream of prime numbers.
+primeS :: Stream Integer
+primeS = cycle primeList
+    where
+        primeList = [x | x <- [2..], isPrime x]
+        isPrime x = null [d | d <- [2..x-1], x `mod` d == 0]
 
 --primetwins :: Stream (Integer,Integer)
 
