e036bb375b8f9a6e8cd482a290c4b41df6d4894c
godarcher <joostgrunwald2001@gmail.com>
2021-12-02 20:06:03 +0100

    finished 11.5.2

--- assignment_11/Result.hs
+++ assignment_11/Result.hs
@@ -5,9 +5,14 @@ import Data.List
 data Result a = Okay a | Error [String]
   deriving (Eq,Ord,Show)
 
+-- Exercise 11.5.1
 instance Functor Result where
-    fmap f (Okay x) = Okay (f x)
-    fmap f (Error [x]) = Error [x]
+  fmap f (Okay x) = Okay (f x)
+  fmap f (Error [x]) = Error [x]
+
+-- Exercise 11.5.2
+-- fmap :: p -> Result a1 -> Result a2
+
 
 --instance Applicative Result where
 --  ...
