f3f07311f389d162cc9bcb6c94631ee786b5fde3
MilanPullens <50156258+MilanPullens@users.noreply.github.com>
2021-12-13 03:25:12 +0100

    Seems to work

--- assignment_12/Dice.hs
+++ assignment_12/Dice.hs
@@ -2,15 +2,18 @@ module AST where
 
 import System.Random
 import RandomState
+import RandomGen
+import Data.List
+import Control.Monad
 
 data Expr = Lit Integer | Dice Integer 
           | Expr :+: Expr
           | Min Expr Expr | Max Expr Expr
-          -- adding and removing -/+
-          | Add Expr Expr | Rem Expr Expr
+          | Expr :-: Expr | Div Expr Expr
   deriving (Show)
 
-infixl 6 :+: 
+infixl 6 :+:
+infixl 6 :-: 
 
 type DiceAction m = Integer -> m Integer
 
@@ -49,8 +52,8 @@ evalM (x :+: y) inp = do
   -- actual output
   return (left + right)
 
--- If we want to add
-evalM (Add a b) da = do
+-- If we want to devide
+evalM (Div a b) da = do
   --first inp
   left <- evalM a da
   -- second inp
@@ -59,7 +62,7 @@ evalM (Add a b) da = do
   return (left `div` right)
 
 -- If we want to remove
-evalM (Rem x y) inp = do
+evalM (x :-: y) inp = do
   -- first inp
   left <- evalM x inp
   -- second inp
@@ -88,14 +91,37 @@ userinp a = do
 
 --evalIO :: Expr -> IO Integer
 
---evalND :: Expr -> [Integer]
+evalND :: Expr -> [Integer]
+evalND e = evalM e f
+  where f n = unfoldr (\x -> if x == n+1 then Nothing else Just (x, x+1)) 1
+
+
 
 avg :: (Fractional a) => [Integer] -> a
 avg xs = fromIntegral (sum xs) / fromIntegral (length xs)
 
---expectation :: (Fractional a) => Expr -> a
---expectation e = avg (evalND e)
+expectation :: (Fractional a) => Expr -> a
+expectation e = avg (evalND e)
 
---evalR :: Expr -> RandomState Integer
+evalR :: Expr -> RandomState Integer
+evalR e = evalM e f2
+  where f2 x = do
+          gen <- get
+          --Use randomR with a stdGen genarator to generate a tuple but only return the random value
+          let (num, r) = randomR (1,x) gen
+          put r
+          return num
 
---observed :: (Fractional a) => Int -> Expr -> IO a
+observed :: (Fractional a) => Int -> Expr -> IO a
+observed n e = do 
+  --This instead of a helper function I could not get to work
+  help <- foldM f4 0 (f3 n e)
+  --help returned the sum of all the rolls, so divide that by the amount of rolls
+  return (fromIntegral help / fromIntegral n)
+    --Roll one dice as long as x < n 
+    where f3 n e = do 
+            unfoldr (\x -> if x == n then Nothing else Just (evalRIO e, x+1)) 0
+          --Add all the values rolled using fold for monads
+          f4 num list = do
+            listVals <- list
+            return (num + listVals)
